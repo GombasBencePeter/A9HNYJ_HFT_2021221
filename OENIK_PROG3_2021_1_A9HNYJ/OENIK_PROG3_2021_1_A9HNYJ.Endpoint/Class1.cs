4b0b871686882ce7d6b67452a6094a863e79e241
gombasBencepeter <gomba000@gmail.com>
2021-10-17 18:30:23 +0200

    Milestone one
    Modified solution structure to meet requirement

---
+++ OENIK_PROG3_2021_1_A9HNYJ/OENIK_PROG3_2021_1_A9HNYJ.Endpoint/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OENIK_PROG3_2021_1_A9HNYJ.Endpoint
+{
+    public class Class1
+    {
+    }
+}
---
+++ OENIK_PROG3_2021_1_A9HNYJ/OENIK_PROG3_2021_1_A9HNYJ.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OENIK_PROG3_2021_1_A9HNYJ.Models
+{
+    public class Class1
+    {
+    }
+}
